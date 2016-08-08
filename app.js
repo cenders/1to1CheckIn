1d634ca3144b35b0e1c8aa0b671503f8fc8bc47b
Benjamin Loong Thomas <benjamin@thomasnetwork.net>
2016-08-08 15:55:12 -0400

    better pagination, active list, and other changes

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ var schema = new mongoose.Schema({
   grade: Number,
   asset: Number,
   openCampus: Boolean,
-  completed: {type: Boolean, default: false}
+  completed: {type: Boolean, default: false},
+  time: String
 },
 {collection: 'dist'});
 
@@ -54,24 +55,29 @@ io.on('connection', function(socket){
 
   //Requesting student list
   socket.on('studentList', function(listObj) {
-    Student.find().limit(listObj.limit).skip(listObj.skip).exec(function(err, objs) {
+    var q = {completed: {$ne: listObj.showIncomplete}};
+
+    Student.find(q).count().exec(function(err, count) {
       if(err) return console.error(err);
 
-      var students = [];
+      Student.find(q).limit(listObj.limit).skip(listObj.skip).exec(function(err, objs) {
+        if(err) return console.error(err);
 
-      for(var i in objs) {
-        students[i] = {
-          id: objs[i].id,
-          name: objs[i].name,
-          grade: objs[i].grade,
-          asset: objs[i].asset,
-          openCampus: objs[i].openCampus
+        var students = [];
+
+        for(var i in objs) {
+          students[i] = {
+            id: objs[i].id,
+            name: objs[i].name,
+            grade: objs[i].grade,
+            asset: objs[i].asset,
+            openCampus: objs[i].openCampus,
+            completed: objs[i].completed
+          }
         }
-      }
 
-      io.emit('list', students);
-
-      console.log('Sending student list');
+        io.emit('list', {count: count, students: students});
+      });
     });
   });
 
@@ -81,18 +87,29 @@ io.on('connection', function(socket){
       if(err) return console.error(err);
 
       // Display all user data
-      console.log(studentObj);
-    });
+      console.log(obj);
 
-    var info = {
-      id: studentObj.id,
-      name: studentObj.name,
-      grade: studentObj.grade,
-      asset: studentObj.asset,
-      openCampus: studentObj.openCampus,
-      completed: studentObj.completed
-    };
-    io.emit('info' , info);
+      var info = {
+        id: obj.id,
+        name: obj.name,
+        grade: obj.grade,
+        asset: obj.asset,
+        openCampus: obj.openCampus,
+        completed: obj.completed
+      };
+      io.emit('info' , info);
+    });
+  });
+
+  socket.on('studentComplete', function(completeObj) {
+    Student.findOneAndUpdate({id: completeObj.id}, {completed: true, time: completeObj.time}).exec(function(err, student) {
+      if(err) return console.error(err);
+      if(!student) return console.error('Student "' + completeObj.id + '" not found, not updated');
+
+      console.log('Student "' + completeObj.id + '" completed');
+
+      io.emit('studentCompleted' , info);
+    });
   });
 });
 
